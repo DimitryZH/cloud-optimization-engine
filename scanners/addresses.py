11ad1c47a59672ab59f96d1f9b16622005ef7ed0
Dmitry Zhuravlev <zhdm.dev@gmail.com>
2026-03-03 09:03:39 -0500

    modify  scanner addresses.py

--- scanners/addresses.py
+++ scanners/addresses.py
@@ -1,19 +1,19 @@
 from google.cloud import compute_v1
 
-def scan_unused_addresses(project_id):
+def scan_unused_static_ips(project_id):
     client = compute_v1.AddressesClient()
     aggregated = client.aggregated_list(project=project_id)
-
-    unused = []
-
+    unused_ips = []
+    
     for region, response in aggregated:
         if response.addresses:
             for address in response.addresses:
-                if not address.users:
-                    unused.append({
+                if not address.allocated:
+                    unused_ips.append({
                         "name": address.name,
-                        "region": region.split("/")[-1],
-                        "address": address.address
+                        "region": region.split('/')[-1],
+                        "ip_address": address.ip_address,
+                        "subnetwork": address.subnetwork.split('/')[-1] if address.subnetwork else None
                     })
-
-    return unused
+    
+    return unused_ips
